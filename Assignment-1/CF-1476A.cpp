7c8b2fae177944b0ad57b00f68b859999d32c4c3
ViditAgarwal2005 <142882884+ViditAgarwal2005@users.noreply.github.com>
2024-05-20 11:02:28 +0530

    Update CF-1476A.c

--- Assignment-1/CF-1476A.cpp
+++ Assignment-1/CF-1476A.cpp
@@ -1,1 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
 
+// already did this question in december
+// used C at that time
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while(t--)
+    {
+        long long n, k;
+        scanf("%lld %lld", &n, &k);
+        if(n<=k)
+        {
+            if(k%n==0)
+                printf("%d\n", k/n);
+            else printf("%d\n", (k/n)+1);
+        }
+        else
+        {
+            int x=n%k;
+            if(x)
+                x=k-(n%k);
+            if(x%n==0)
+                printf("%d\n", (x/n)+1);
+            else
+                printf("%d\n", (x/n)+2);
+        }
+
+    }
+    return 0;
+}
